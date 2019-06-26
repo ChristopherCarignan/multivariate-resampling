fd4a000721ce2b512c7e9dd5dc9714b2e1ecb341
christophercarignan <carignanc@gmail.com>
2019-06-26 11:23:25 +0200

    Optimization of duplicate checking

--- multivar_resamp.r
+++ multivar_resamp.r
@@ -1,5 +1,5 @@
 # Filename: multivar_resamp.r
-# Date: 2019-06-25
+# Date: 2019-06-26
 # Author: Christopher Carignan
 # Email: c.carignan@phonetik.uni-muenchen.de
 # Institution: Institute of Phonetics and Speech Processing (IPS), Ludwig-Maximilians-Universität München, Munich, Germany
@@ -19,37 +19,39 @@
 require(pracma)
 
 multivar_resamp <- function (inputdata, groupby, resampnum, features) {
-  
+
   outputdata <- c()
   for (group in unique(inputdata[[groupby]])) {
     subdata <- inputdata[inputdata[[groupby]]==group,]
     
     if (nrow(subdata) > resampnum) { # more samples than the desired number?
       # undersample to the desired number
-      trimmed <- subdata[sample(nrow(subdata), resampnum, replace=F), ]
+      newdata <- subdata[sample(nrow(subdata), resampnum, replace=F), ]
       
       # print a success message
-      print(paste0("'",groupby,"' level named '", group, "' has been down-sampled from ", nrow(subdata), " to ", resampnum, " observations"), quote=F)
+      print(paste0("'",groupby,"' level named '", group, "' has been under-sampled from ", nrow(subdata), " to ", resampnum, " observations"), quote=F)
+      
     } else {
       if (nrow(subdata) == resampnum) { # same number of samples as the desired number?
         # keep the original data as-is
-        trimmed <- subdata
+        newdata <- subdata
         
         # print a success message
         print(paste0("'",groupby,"' level named '", group, "' has been kept at ", nrow(subdata), " observations"), quote=F)
+        
       } else { # fewer samples than the desired number?
         # oversample!
-        oversamp              <- resampnum - nrow(subdata) # number of samples to use in oversampling
-        trimmed               <- subdata # original data to add new observations to
+        oversamp              <- resampnum - nrow(subdata) # number of new observations to use in oversampling
         sub.scaled            <- subdata # original data to scale
         means                 <- apply(sub.scaled[,features], 2, mean) # get the original feature means
         stdevs                <- apply(sub.scaled[,features], 2, sd) # get the original feature standard deviations
         sub.scaled[,features] <- scale(subdata[,features]) # scale the original features
+        oversamp.data         <- c() # oversampled data to build
         
         for (samp in 1:oversamp) {
           # randomly choose an observation from the scaled feature matrix
           this.samp   <- sub.scaled[sample(nrow(sub.scaled), 1), ]
-          # select all of the other observations from the scaled feature matrix
+          # select all of the OTHER observations from the scaled feature matrix
           other.samps <- sub.scaled[which(row.names(sub.scaled)!=row.names(this.samp)), ]
           
           # calculate Euclidean distances between the selected observation and all other observations
@@ -58,8 +60,8 @@ multivar_resamp <- function (inputdata, groupby, resampnum, features) {
           # number of nearest neighbors = 1% of total number of original observations
           neighbors <- sort(dists)[1:round(nrow(subdata) * 0.01)]
           
+          # while loop which ensures that no duplicate observations are created in the oversampling process
           n.check <- 0
-          
           while (n.check == 0) {
             # randomly select one of the nearest neighbors
             n.dist    <- sample(neighbors, 1)
@@ -79,8 +81,8 @@ multivar_resamp <- function (inputdata, groupby, resampnum, features) {
             # replace old features with new features
             this.samp[,features] <- new.features
             
-            # check if it is a duplicate observation
-            dup.check <- duplicated(rbind(trimmed,this.samp))
+            # check if it is a duplicate oversampled observation
+            dup.check <- duplicated(rbind(oversamp.data,this.samp))
             
             # if it is NOT a duplicate, exit the loop and add it to the data frame
             # if it IS a duplicate, run this loop again with a different nearest neighbor
@@ -88,16 +90,18 @@ multivar_resamp <- function (inputdata, groupby, resampnum, features) {
               n.check <- 1
             }
           }
-          
           # add new observation to data frame
-          trimmed <- rbind(trimmed,this.samp)
+          oversamp.data <- rbind(oversamp.data,this.samp)
         }
+        # add oversampled data to original data frame
+        newdata <- rbind(subdata,oversamp.data)
+        
         # print a success message
-        print(paste0("'",groupby,"' level named '", group, "' has been up-sampled from ", nrow(subdata), " to ", resampnum, " observations"), quote=F)
+        print(paste0("'",groupby,"' level named '", group, "' has been over-sampled from ", nrow(subdata), " to ", resampnum, " observations"), quote=F)
       } 
     }
     # add resampled data to output dataset
-    outputdata <- rbind(outputdata,trimmed)
+    outputdata <- rbind(outputdata,newdata)
   }
   return(outputdata)
 }
