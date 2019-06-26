4d653f78f20d2252e6d86661815523f601f93293
christophercarignan <carignanc@gmail.com>
2019-06-26 15:36:37 +0200

    Added example function call

--- multivar_resamp.r
+++ multivar_resamp.r
@@ -14,6 +14,8 @@
 #   outputdata: data frame with resampled observations; 
 #               every level of 'groupby' will contain a number of observations equal to 'resampnum'
 
+# Example function call:
+# outputdat <- multivar_resamp(inputdat, "speaker", 5000, c("F1","F2","F3","P0","P1"))
 
 # Load required libraries
 require(pracma)
